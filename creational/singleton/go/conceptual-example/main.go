093c4b49fbd706dd5d4f20903e8bb60f40144cb7
Ali Nouri <alli.nouri.83@gmail.com>
2026-05-10 20:16:15 +0330

    singleton

---
+++ creational/singleton/go/conceptual-example/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "sync"
+
+func main() {
+	wg := new(sync.WaitGroup)
+	for i := 0; i < 30; i++ {
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			getInstance()
+		}()
+	}
+
+	wg.Wait()
+}
